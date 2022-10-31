43f3c81da217ccf68e25aada9278d9079579a9e2
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-10-31 16:42:09 +0100

    created database

---
+++ Elearn.Data/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Elearn.Data.Data;
+
+public class DataContext : DbContext
+{
+    public string CONNECTION { get; set; } = "Data Source=elearn.db";
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(CONNECTION);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        Database.Migrate();
+    }
+    
+    
+}
