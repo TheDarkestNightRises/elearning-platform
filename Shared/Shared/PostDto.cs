8c2b2f8c68d011f53f21119435ca9451e7bda677
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-11-03 14:57:26 +0100

    created post dtos
    dto and dto for creation

---
+++ Shared/Shared/PostDto.cs
@@ -0,0 +1,30 @@
+﻿using Shared.Models;
+
+namespace Shared;
+
+public class PostDto
+{
+    public int Id { get; set; }
+    public string Title{ get; set; }
+    public string Body{ get; set; }
+    public string Url{ get; set; } 
+    public string Image { get; set; }
+    public User Author{ get; set; }
+    public DateTime DateCreated { get; set; }
+
+    public PostDto(int id, string title, string body, string url, string image, User author, DateTime dateCreated)
+    {
+        Id = id;
+        Title = title;
+        Body = body;
+        Url = url;
+        Image = image;
+        Author = author;
+        DateCreated = dateCreated;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id} {Title} {Body} {Url}";
+    }
+}
