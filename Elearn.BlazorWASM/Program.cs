c7a1a38c0b716207f0e183326a5a3220714cd8c1
Lichiag21 <lichiag21@gmail.com>
2022-11-06 00:41:06 +0100

    commens create done

--- Elearn.BlazorWASM/Program.cs
+++ Elearn.BlazorWASM/Program.cs
@@ -8,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<IPostService, PostHttpClient>();
+builder.Services.AddScoped<ICommentService, CommentHttpClient>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7219") });
 builder.Services.AddMudServices();
 
