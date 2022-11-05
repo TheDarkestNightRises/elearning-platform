9a156f5230fef4dce26b94894c2cc4863212d6b4
TheDarkestNightRises <icecold363@gmail.com>
2022-11-05 21:02:03 +0100

    Added client side fetch all posts

--- Elearn.BlazorWASM/Program.cs
+++ Elearn.BlazorWASM/Program.cs
@@ -1,3 +1,4 @@
+using Blog.BlazorWASM;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,8 +8,8 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IPostService, PostHttpClient>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7219") });
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
