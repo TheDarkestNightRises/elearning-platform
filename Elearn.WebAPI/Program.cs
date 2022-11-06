32d0bd9c33a204ef57180d6333a2101be6590a20
TheDarkestNightRises <icecold363@gmail.com>
2022-11-06 19:03:03 +0100

    Setup for auth rest api

--- Elearn.WebAPI/Program.cs
+++ Elearn.WebAPI/Program.cs
@@ -1,8 +1,12 @@
+using System.Text;
 using Elearn.Application.Logic;
 using Elearn.Application.LogicInterfaces;
 using Elearn.Application.RepositoryInterfaces;
 using Elearn.Data.Data;
 using Elearn.Data.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Shared.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +22,23 @@ builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<ICommentLogic, CommentLogic>();
 builder.Services.AddScoped<IPostLogic, PostLogic>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 builder.Services.AddScoped<IAuthLogic, AuthLogic>();
+AuthorizationPolicies.AddPolicies(builder.Services);
+
 
 
 var app = builder.Build();
@@ -33,6 +53,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseHttpsRedirection();
 
