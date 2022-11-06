f33f2a985c1e4890f409ef17c7d8382409653b77
TheDarkestNightRises <icecold363@gmail.com>
2022-11-06 20:22:27 +0100

    Created custom auth provider

---
+++ Elearn.BlazorWASM/Auth/CustomAuthProvider.cs
@@ -0,0 +1,31 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Elearn.BlazorWASM.Auth;
+
+public class CustomAuthProvider : AuthenticationStateProvider
+{
+    private readonly IAuthService authService;
+
+    public CustomAuthProvider(IAuthService authService)
+    {
+        this.authService = authService;
+        authService.OnAuthStateChanged += AuthStateChanged;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        ClaimsPrincipal principal = await authService.GetAuthAsync();
+
+        return new AuthenticationState(principal);
+    }
+
+    private void AuthStateChanged(ClaimsPrincipal principal)
+    {
+        NotifyAuthenticationStateChanged(
+            Task.FromResult(
+                new AuthenticationState(principal)
+            )
+        );
+    }
+}
--- Elearn.BlazorWASM/Program.cs
+++ Elearn.BlazorWASM/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Elearn.BlazorWASM;
+using Elearn.BlazorWASM.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +11,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<IPostService, PostHttpClient>();
 builder.Services.AddScoped<ICommentService, CommentHttpClient>();
+builder.Services.AddScoped<IAuthService, JwtHttpClient>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7219") });
 builder.Services.AddMudServices();
 
