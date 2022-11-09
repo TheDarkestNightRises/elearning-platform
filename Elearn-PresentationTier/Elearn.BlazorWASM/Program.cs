1d959b3f218f61fc7b6d50064c5d3930b0d4ee14
TheDarkestNightRises <icecold363@gmail.com>
2022-11-09 21:06:10 +0100

    Renamed Blazor wasm

--- Elearn-PresentationTier/Elearn.BlazorWASM/Program.cs
+++ Elearn-PresentationTier/Elearn.BlazorWASM/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorWASM;
+using Elearn.BlazorWASM;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
