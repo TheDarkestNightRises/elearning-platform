b96e907378bd61af4d08da6679ebb74e21775192
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-11-14 17:36:47 +0100

    Added entities

---
+++ Elearn-DataTier/src/main/java/via/dk/elearn/entity/Course.java
@@ -0,0 +1,16 @@
+package via.dk.elearn.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "course_name", length = 30)
+    private String courseName;
+}
---
+++ Elearn-DataTier/src/main/java/via/dk/elearn/entity/Question.java
@@ -0,0 +1,32 @@
+package via.dk.elearn.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title", length = 50)
+    private String title;
+
+    @Column(name = "description", length = 80)
+    private String description;
+
+    @Lob
+    @Column(name = "body")
+    private String body;
+
+    //TODO: Does question have course?
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
