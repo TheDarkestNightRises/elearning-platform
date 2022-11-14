9522bb717a3f6f6ea0d97f989934bebc2c49f123
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-11-14 17:39:21 +0100

    Added seed classes with one example
    Co-Authored-By: Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
    Co-Authored-By: Andrei-George Lichi <93666980+Andrei-Lichi@users.noreply.github.com>
    Co-Authored-By: Oriana11 <85575367+Oriana11@users.noreply.github.com>
    Co-Authored-By: kotruta123 <95081082+kotruta123@users.noreply.github.com>

---
+++ Elearn-DataTier/src/main/java/via/dk/elearn/seed/TeacherSeed.java
@@ -0,0 +1,27 @@
+package via.dk.elearn.seed;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import via.dk.elearn.entity.Country;
+import via.dk.elearn.entity.Teacher;
+import via.dk.elearn.repository.CountryRepository;
+import via.dk.elearn.repository.TeacherRepository;
+
+@Component
+public class TeacherSeed implements CommandLineRunner {
+
+    @Autowired
+    TeacherRepository repo;
+    @Autowired
+    CountryRepository countryRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        Country country = new Country();
+        country.setCountryName("Denmark");
+        Country created = countryRepository.save(country);
+        Teacher teacher = new Teacher("user", "gggg@sfus.3w4", "passsdd", "asfaf", "ajsnsa", created, 123l);
+        repo.save(teacher);
+
+    }
+}
---
+++ Elearn-DataTier/src/main/java/via/dk/elearn/seed/UniversitySeed.java
@@ -0,0 +1,4 @@
+package via.dk.elearn.seed;
+
+public class UniversitySeed {
+}
