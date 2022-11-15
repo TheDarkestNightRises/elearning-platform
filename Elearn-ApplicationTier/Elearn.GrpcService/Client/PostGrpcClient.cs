9d9c38314f95a45e9c6c400647215572b531b7ae
TheDarkestNightRises <icecold363@gmail.com>
2022-11-15 13:31:24 +0100

    Grpc client example

--- Elearn-ApplicationTier/Elearn.GrpcService/Client/PostGrpcClient.cs
+++ Elearn-ApplicationTier/Elearn.GrpcService/Client/PostGrpcClient.cs
@@ -1,18 +1,37 @@
 using Elearn.Application.ServiceContracts;
-using Elearn.Shared.Models;
+using ElearnGrpc;
+using Grpc.Net.Client;
+using Post = Elearn.Shared.Models.Post;
 
 namespace Elearn.GrpcService.Client;
 
 public class PostGrpcClient : IPostService
 {
+    private GrpcChannel _grpcChannel;
+    private ElearnGrpc.Post.PostClient _postClient;
+
+    public PostGrpcClient()
+    {
+        _grpcChannel = GrpcChannel.ForAddress("https://localhost:7261");
+        _postClient = new ElearnGrpc.Post.PostClient(_grpcChannel);
+    }
+
     public Task<List<Post>> GetAllPostsAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task<Post?> GetPostAsync(string url)
+    public async Task<Post?> GetPostAsync(string url)
     {
-        throw new NotImplementedException();
+        var postRequested = new PostLookupModel { Url = url };
+        var postFromGrpc = _postClient.GetPost(postRequested);
+        Post post = new Post
+        {
+            Id = postFromGrpc.Id,
+            Body = postFromGrpc.Body,
+            Title = postFromGrpc.Title
+        };
+        return post;
     }
 
     public Task<Post> CreateNewPostAsync(Post post)
