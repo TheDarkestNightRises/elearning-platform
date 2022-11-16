4144014c37407fa5812240389ac97c3081b04df5
kotruta123 <315175@viauc.dk>
2022-11-16 03:23:00 +0100

    added user proto

--- Elearn-ApplicationTier/Elearn.GrpcService/Client/UserGrpcClient.cs
+++ Elearn-ApplicationTier/Elearn.GrpcService/Client/UserGrpcClient.cs
@@ -1,13 +1,18 @@
 ﻿using Elearn.Application.ServiceContracts;
 using Elearn.Shared.Models;
+using Grpc.Net.Client;
 
 namespace Elearn.GrpcService.Client;
 
 public class UserGrpcClient : IUserService
 {
+    private GrpcChannel _grpcChannel;
+    
+    
     public Task<User?> GetUserByNameAsync(string name)
     {
         throw new NotImplementedException();
+
     }
 
     public Task<User> CreateNewUserAsync(User user)
