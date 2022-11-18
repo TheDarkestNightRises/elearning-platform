b3db31d48882e36207164a734208b1fb21277961
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-11-18 22:29:16 +0100

    Added missing queries
    added queries:
    -find lectures upvoted by a user
    -count upvotes/downvotes of post

--- Elearn-DataTier/src/main/java/via/dk/elearn/repository/LectureRepository.java
+++ Elearn-DataTier/src/main/java/via/dk/elearn/repository/LectureRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface LectureRepository  extends JpaRepository<Lecture,Long> {
     @Query(value = "SELECT * FROM lecture l WHERE l.url = :url", nativeQuery = true)
     List<Lecture> findByUrl(String url);
+    @Query(value = "SELECT * FROM lecture l WHERE l.id IN (SELECT lecture_id FROM lecture_vote lv WHERE user_id = :userId AND lv.upvote = true)", nativeQuery = true)
+    List<Lecture> findUpvotedLecturesOfUser(Long userId);
 }
--- Elearn-DataTier/src/main/java/via/dk/elearn/repository/LectureVoteRepository.java
+++ Elearn-DataTier/src/main/java/via/dk/elearn/repository/LectureVoteRepository.java
@@ -1,10 +1,15 @@
 package via.dk.elearn.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import via.dk.elearn.models.Lecture;
 import via.dk.elearn.models.LectureVoteId;
 import via.dk.elearn.models.LectureVote;
 
+import java.util.List;
+
 @Repository
 public interface LectureVoteRepository extends JpaRepository<LectureVote, LectureVoteId> {
+    Long countLectureVotesByUpvoteEqualsAndLectureEquals(boolean vote, Lecture lecture);//
 }
