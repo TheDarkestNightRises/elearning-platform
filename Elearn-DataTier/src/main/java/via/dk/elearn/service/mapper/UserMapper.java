d1339a67390e8a1d12455c250ba1ac9c541a0f7a
TheDarkestNightRises <icecold363@gmail.com>
2022-11-20 10:12:45 +0100

    User to grpc model

--- Elearn-DataTier/src/main/java/via/dk/elearn/service/mapper/UserMapper.java
+++ Elearn-DataTier/src/main/java/via/dk/elearn/service/mapper/UserMapper.java
@@ -1,4 +1,19 @@
 package via.dk.elearn.service.mapper;
 
+import via.dk.elearn.models.User;
+import via.dk.elearn.protobuf.UserModel;
+
 public class UserMapper {
+    public static UserModel convertUserToGrpcModel(User user) {
+        return UserModel.newBuilder()
+                .setId(user.getId())
+                .setUsername(user.getUsername())
+                .setEmail(user.getEmail())
+                .setName(user.getName())
+                .setPassword(user.getPassword())
+                .setRole(user.getRole())
+                .setSecurityLevel(user.getSecurity_level())
+                .build();
+    }
+
 }
