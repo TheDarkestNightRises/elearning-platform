608a1abd66475a9f5aceddb1fdd7775de66381e4
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-11-23 00:34:00 +0100

    Added Lecture Vote Entity in data tier
    lecture vote entity and related classes

---
+++ Elearn-ApplicationTier/Elearn.Shared/Models/LectureVoteCounter.cs
@@ -0,0 +1,15 @@
+﻿namespace Elearn.Shared.Models;
+
+public class LectureVoteCounter
+{
+    public Post Post { set; get; }
+    public long upvoteCount{ set; get; }
+    public long downvoteCount{ set; get; }
+
+    public LectureVoteCounter(Post post, long upvoteCount, long downvoteCount)
+    {
+        Post = post;
+        this.upvoteCount = upvoteCount;
+        this.downvoteCount = downvoteCount;
+    }
+}
