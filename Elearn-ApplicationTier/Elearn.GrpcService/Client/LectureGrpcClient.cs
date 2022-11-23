b0f2a5f2f6198b2343fec747ea7a8fcb3153a6bc
TheDarkestNightRises <icecold363@gmail.com>
2022-11-23 01:02:54 +0100

    Changed client in application

--- Elearn-ApplicationTier/Elearn.GrpcService/Client/LectureGrpcClient.cs
+++ Elearn-ApplicationTier/Elearn.GrpcService/Client/LectureGrpcClient.cs
@@ -1,59 +1,59 @@
-// using Elearn.Application.ServiceContracts;
-// using Elearn.GrpcService.Extensions;
-// using Elearn.Shared.Models;
-// using ElearnGrpc;
-// using Grpc.Core;
-// using Grpc.Net.Client;
-// using Grpc.Net.Client.Web;
-//
-// namespace Elearn.GrpcService.Client;
-//
-// public class LectureGrpcClient : ILectureService
-// {
-//     private PostService.PostServiceClient _postClient;
-//
-//     public LectureGrpcClient()
-//     {
-//         var _grpcChannel =
-//             new Channel("localhost:8843", ChannelCredentials.Insecure);
-//         _postClient = new PostService.PostServiceClient(_grpcChannel);
-//     }
-//
-//     public async Task<List<Lecture>> GetAllPostsAsync()
-//     {
-//         List<Lecture> posts = new List<Lecture>();
-//         using (var call = _postClient.GetAllPost(new NewPostRequest()))
-//         {
-//             while (await call.ResponseStream.MoveNext())
-//             {
-//                 var currentPost = call.ResponseStream.Current;
-//                 posts.Add(currentPost.AsBase());
-//             }
-//         }
-//         return posts;
-//     }
-//
-//     public async Task<Lecture?> GetPostAsync(string url)
-//     {
-//         var postRequested = new PostUrl { Url = url };
-//         var postFromGrpc = await _postClient.GetPostAsync(postRequested);
-//         return postFromGrpc.AsBase();
-//     }
-//
-//     public async Task<Lecture> CreateNewPostAsync(Lecture lecture)
-//     {
-//         var postModel = post.AsGrpcModel();
-//         //Append user to post
-//         var userModel = post.Author.AsGrpcModel();
-//         postModel.User = userModel;
-//         var createdPostFromGrpc = await _postClient.CreateNewPostAsync(postModel);
-//         return createdPostFromGrpc.AsBase();
-//     }
-//     
-//     public async Task<Lecture?> GetByIdAsync(int id)
-//     {
-//         var postRequested = new PostId { Id = dtoPostId };
-//         var postFromGrpc = await _postClient.GetByIdAsync(postRequested);
-//         return postFromGrpc.AsBase();    
-//     }
-// }
+using Elearn.Application.ServiceContracts;
+using Elearn.GrpcService.Extensions;
+using Elearn.Shared.Models;
+using ElearnGrpc;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+namespace Elearn.GrpcService.Client;
+
+public class LectureGrpcClient : ILectureService
+{
+    private LectureService.LectureServiceClient _lectureClient;
+
+    public LectureGrpcClient()
+    {
+        var _grpcChannel =
+            new Channel("localhost:8843", ChannelCredentials.Insecure);
+        _lectureClient = new LectureService.LectureServiceClient(_grpcChannel);
+    }
+
+    public async Task<List<Lecture>> GetAllPostsAsync()
+    {
+        List<Lecture> lectures = new List<Lecture>();
+        using (var call = _lectureClient.GetAllLectures(new NewLectureRequest()))
+        {
+            while (await call.ResponseStream.MoveNext())
+            {
+                var currentLecture = call.ResponseStream.Current;
+                lectures.Add(currentLecture.AsBase());
+            }
+        }
+        return lectures;
+    }
+
+    public async Task<Lecture?> GetPostAsync(string url)
+    {
+        var lectureRequested = new LectureUrl { Url = url };
+        var postFromGrpc = await _lectureClient.GetLectureAsync(lectureRequested);
+        return postFromGrpc.AsBase();
+    }
+
+    public async Task<Lecture> CreateNewPostAsync(Lecture lecture)
+    {
+        var postModel = lecture.AsGrpcModel();
+        //Append user to post
+        var userModel = lecture.Author.AsGrpcModel();
+        postModel.User = userModel;
+        var createdPostFromGrpc = await _lectureClient.CreateNewPostAsync(postModel);
+        return createdPostFromGrpc.AsBase();
+    }
+    
+    public async Task<Lecture?> GetByIdAsync(int id)
+    {
+        var lectureRequested = new LectureId { Id = id };
+        var lectureGrpcModel = await _lectureClient.GetLectureByIdAsync(lectureRequested);
+        return lectureGrpcModel.AsBase();    
+    }
+}
--- Elearn-ApplicationTier/Elearn.GrpcService/Extensions/GrpcLectureExtension.cs
+++ Elearn-ApplicationTier/Elearn.GrpcService/Extensions/GrpcLectureExtension.cs
@@ -1,32 +1,33 @@
-﻿// using Elearn.Shared.Dtos;
-// using Elearn.Shared.Models;
-//
-// namespace Elearn.GrpcService.Extensions;
-//
-// public static class GrpcLectureExtension
-// {
-//     public static LectureModel AsGrpcModel(this Lecture lecture)
-//     {
-//         return new PostModel
-//         {
-//             Id = post.Id,
-//             Url = post.Url,
-//             Image = post.Image,
-//             Title = post.Title,
-//             Body = post.Body,
-//         };
-//     }
-//
-//     public static Post AsBase(this PostModel postModel)
-//     {
-//         return new Post
-//         {
-//             Id = postModel.Id,
-//             Url = postModel.Url,
-//             Image = postModel.Image,
-//             Title = postModel.Title,
-//             Body = postModel.Body
-//         };
-//     }
-// }
-//
+﻿using Elearn.Shared.Dtos;
+using Elearn.Shared.Models;
+using ElearnGrpc;
+
+namespace Elearn.GrpcService.Extensions;
+
+public static class GrpcLectureExtension
+{
+    public static LectureModel AsGrpcModel(this Lecture lecture)
+    {
+        return new LectureModel
+        {
+            Id = lecture.Id,
+            Url = lecture.Url,
+            Image = lecture.Image,
+            Title = lecture.Title,
+            Body = lecture.Body,
+        };
+    }
+
+    public static Lecture AsBase(this LectureModel lectureModel)
+    {
+        return new Lecture
+        {
+            Id = lectureModel.Id,
+            Url = lectureModel.Url,
+            Image = lectureModel.Image,
+            Title = lectureModel.Title,
+            Body = lectureModel.Body
+        };
+    }
+}
+
