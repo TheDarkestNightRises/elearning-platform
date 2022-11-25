957d4bf0d8072bd0812d4f695aa90f66c3ad86e1
Oriana11 <rubyjpy@gmail.com>
2022-11-25 16:56:24 +0100

    Made the web API for search

---
+++ Elearn-ApplicationTier/Elearn.WebAPI/Controllers/SearchController.cs
@@ -0,0 +1,64 @@
+using Elearn.Application.LogicInterfaces;
+using Elearn.Shared.Dtos;
+using Elearn.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Extensions;
+
+namespace Elearn.WebAPI.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class SearchController : ControllerBase
+{
+    private readonly ISearchLogic _searchLogic;
+    
+    public SearchController(ISearchLogic searchLogic)
+    {
+        _searchLogic = searchLogic;
+    }
+
+    /*[HttpGet("/user/{name}")]
+    public async Task<ActionResult<List<UserDto>>> SearchUsers(string name)
+    {
+        try
+        {
+            var result = await _searchLogic.SearchUsersAsync(name);
+            return Ok(result.AsDtos());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+        
+    }*/
+    
+    [ HttpGet("/question/{title}")]
+    public async Task<ActionResult<List<QuestionDto>>> SearchQuestions(string title)
+    {
+        try
+        {
+            var result = await _searchLogic.SearchQuestionsAsync(title);
+            return Ok(result.AsDtos());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [ HttpGet("/lecture/{title}")]
+    public async Task<ActionResult<List<LectureDto>>> SearchLecture(string title)
+    {
+        try
+        {
+            var result = await _searchLogic.SearchLecturesAsync(title);
+            return Ok(result.AsDtos());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+    }
+}
