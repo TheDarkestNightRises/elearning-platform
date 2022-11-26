13b8c23d1edd872cd8374fdcef65028a57ace252
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-11-26 23:04:26 +0100

    Improved grpc lecture vote and added logic layer for lecture vote

---
+++ Elearn-ApplicationTier/Elearn.Application/Logic/LectureVoteLogic.cs
@@ -0,0 +1,67 @@
+﻿using Elearn.Application.LogicInterfaces;
+using Elearn.Application.ServiceContracts;
+using Elearn.Shared.Models;
+
+namespace Elearn.Application.Logic;
+
+public class LectureVoteLogic : ILectureVoteLogic
+{
+    private readonly ILectureService _lectureService;
+    private readonly IUserService _userService;
+    private readonly ILectureVoteService _lectureVoteService;
+
+    public LectureVoteLogic(ILectureService lectureService, IUserService userService, ILectureVoteService lectureVoteService)
+    {
+        _lectureService = lectureService;
+        _userService = userService;
+        _lectureVoteService = lectureVoteService;
+    }
+    public async Task<LectureVote> CreateLectureVoteAsync(LectureVote lectureVote)
+    {
+        if (lectureVote.User is null)
+        {
+            throw new Exception("Invalid request: user not present in request");
+        }
+        User? user = await _userService.GetUserByUsernameAsync(lectureVote.User.Username);
+        if (user is null)
+        {
+            throw new Exception("User not found in database");
+        }
+        
+        if (lectureVote.Lecture is null)
+        {
+            throw new Exception("Invalid request: lecture not present in request");
+        }
+        Lecture? lecture = await _lectureService.GetPostAsync(lectureVote.Lecture.Url);
+        if (lecture is null)
+        {
+            throw new Exception("Lecture not found in database");
+        }
+
+        if (lectureVote.Upvote is null)
+        {
+            throw new Exception("Vote type not set");
+        }
+        LectureVote lectureVoteRequest = new LectureVote(user, lecture, lectureVote.Upvote);
+        return await _lectureVoteService.CreateLectureVoteAsync(lectureVoteRequest);
+    }
+
+    public async Task<LectureVoteCounter> GetLectureVotesCountAsync(Lecture post)
+    {
+        return await _lectureVoteService.GetLectureVotesCountAsync(post);
+    }
+
+    public async Task<LectureVote> GetLectureVotebyIdAsync(User user, Lecture post)
+    {
+        Lecture? requestedLecture = await _lectureService.GetPostAsync(post.Url);
+        User? requestedUser = await _userService.GetUserByUsernameAsync(user.Username);
+        if (requestedLecture is null || requestedUser is null)
+        {
+            throw new Exception("Invalid request: user or lecture not found");
+        }
+        
+        LectureVote lectureVote = await _lectureVoteService.GetLectureVotebyIdAsync(user, post);
+
+        return await _lectureVoteService.GetLectureVotebyIdAsync(requestedUser, requestedLecture);
+    }
+}
--- Elearn-ApplicationTier/Elearn.Shared/Models/LectureVote.cs
+++ Elearn-ApplicationTier/Elearn.Shared/Models/LectureVote.cs
@@ -2,14 +2,19 @@
 
 public class LectureVote
 {
-    public User User { get; set; }
-    public Lecture Lecture { get; set; }
-    public bool Upvote { get; set; }
+    public User? User { get; set; }
+    public Lecture? Lecture { get; set; }
+    public bool? Upvote { get; set; }
 
-    public LectureVote(User user, Lecture lecture, bool upvote)
+    public LectureVote(User? user, Lecture? lecture, bool? upvote)
     {
         User = user;
         Lecture = lecture;
         Upvote = upvote;
+    }
+
+    public LectureVote()
+    {
+        
     }
 }
