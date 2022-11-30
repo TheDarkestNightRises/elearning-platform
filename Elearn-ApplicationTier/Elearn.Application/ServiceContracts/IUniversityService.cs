37aee909094a6b3c8e2b2b20affb5488f51d46a9
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-11-30 20:26:20 +0100

    Added university grpc client

---
+++ Elearn-ApplicationTier/Elearn.Application/ServiceContracts/IUniversityService.cs
@@ -0,0 +1,9 @@
+﻿using Elearn.Shared.Models;
+
+namespace Elearn.Application.ServiceContracts;
+
+public interface IUniversityService
+{
+    Task<List<Lecture>> GetAllLecturesByUniversity(University university);
+    Task<List<University>> GetAllUniversities();
+}
---
+++ Elearn-ApplicationTier/Elearn.GrpcService/Client/UniversityGrpcClient.cs
@@ -0,0 +1,46 @@
+﻿using Elearn.Application.ServiceContracts;
+using Elearn.GrpcService.Extensions;
+using Elearn.Shared.Models;
+using Grpc.Core;
+
+namespace Elearn.GrpcService.Client;
+
+public class UniversityGrpcClient : IUniversityService
+{
+    private UniversityService.UniversityServiceClient _universityClient;
+
+    public UniversityGrpcClient()
+    {
+        var _grpcChannel = new Channel("localhost:8843", ChannelCredentials.Insecure);
+        _universityClient = new UniversityService.UniversityServiceClient(_grpcChannel);
+    }
+
+    public async Task<List<Lecture>> GetAllLecturesByUniversity(University university)
+    {
+        List<Lecture> lectures = new List<Lecture>();
+        using (var call = _universityClient.GetLecturesByUniversity(university.AsGrpcModel()))
+        {
+            while (await call.ResponseStream.MoveNext())
+            {
+                var currentLecture = call.ResponseStream.Current;
+                lectures.Add(currentLecture.AsBase());
+            }
+        }
+
+        return lectures;
+    }
+
+    public async Task<List<University>> GetAllUniversities()
+    {
+        List<University> universities = new List<University>();
+        using (var call = _universityClient.GetUniversities(new UniversityRequest()))
+        {
+            while (await call.ResponseStream.MoveNext())
+            {
+                var currentUniversity = call.ResponseStream.Current;
+                universities.Add(currentUniversity.AsBase());
+            }
+        }
+        return universities;
+    }
+}
