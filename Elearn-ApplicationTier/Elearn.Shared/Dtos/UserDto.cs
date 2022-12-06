73c619c19afa477cfaaf96d9025af1dc9c1bf717
kotruta123 <315175@viauc.dk>
2022-12-06 17:33:50 +0100

    Logout

---
+++ Elearn-ApplicationTier/Elearn.Shared/Dtos/UserDto.cs
@@ -0,0 +1,40 @@
+﻿namespace Elearn.Shared.Dtos;
+
+public class UserDto
+{
+    public int UserId { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string Image { get; set; }
+
+    public string Role { get; set; }
+
+    public int SecurityLevel
+    {
+        get; set;
+    }
+    public bool Approved { get; set; }
+    
+    
+    public UserDto(string username, string password, string email, string name, string role, int securityLevel, string image, bool approved)
+    {
+        Username = username;
+        Password = password;
+        Email = email;
+        Name = name;
+        Role = role;
+        SecurityLevel = securityLevel;
+        Approved = approved;
+        Image = image;
+    }
+    public UserDto()
+    {
+    }
+    
+    public override string ToString()
+    {
+        return $"{UserId} {Name}";
+    }
+}
