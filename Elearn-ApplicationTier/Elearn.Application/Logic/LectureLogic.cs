fb6c87f0c0de1f12fa142c6f6d01caf4ca4975ef
Lichiag21 <lichiag21@gmail.com>
2022-12-07 02:34:22 +0100

    add lecture fixed

--- Elearn-ApplicationTier/Elearn.Application/Logic/LectureLogic.cs
+++ Elearn-ApplicationTier/Elearn.Application/Logic/LectureLogic.cs
@@ -36,7 +36,7 @@ public class LectureLogic : ILectureLogic
             throw new Exception("This URL is already used");
         }
         
-        Lecture lectureAppended = new Lecture(lecture.Title, lecture.Body, lecture.Url, lecture.Image,lecture.Description, teacher);
+        Lecture lectureAppended = new Lecture(lecture.Url, lecture.Image, lecture.Title, lecture.Body,lecture.Description, teacher);
         Lecture created = await _lectureService.CreateNewPostAsync(lectureAppended);
         return created;
     }
