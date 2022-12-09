c738e3eb90f11a4d20675b48e6c73d70942df7e1
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-12-09 01:11:36 +0100

    Added country grpc service in data tier

---
+++ Elearn-DataTier/src/main/java/via/dk/elearn/service/CountryServiceImpl.java
@@ -0,0 +1,65 @@
+package via.dk.elearn.service;
+
+import com.google.protobuf.Any;
+import com.google.rpc.ErrorInfo;
+import io.grpc.protobuf.StatusProto;
+import io.grpc.stub.StreamObserver;
+import via.dk.elearn.models.Country;
+import via.dk.elearn.models.University;
+import via.dk.elearn.protobuf.*;
+import via.dk.elearn.repository.CountryRepository;
+import via.dk.elearn.service.mapper.CountryMapper;
+import via.dk.elearn.service.mapper.UniversityMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CountryServiceImpl extends CountryServiceGrpc.CountryServiceImplBase {
+
+    private CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public void getCountries(CountryRequest request, StreamObserver<CountryModel> responseObserver) {
+        List<Country> countries = countryRepository.findAll();
+        if (countries.isEmpty()) {
+            com.google.rpc.Status status = com.google.rpc.Status.newBuilder()
+                    .setCode(com.google.rpc.Code.NOT_FOUND.getNumber())
+                    .setMessage("No countries were not found")
+                    .addDetails(Any.pack(ErrorInfo.newBuilder()
+                            .setReason("Countries not found")
+                            .build()))
+                    .build();
+            responseObserver.onError(StatusProto.toStatusRuntimeException(status));
+            return;
+        }
+        for (Country country : countries) {
+            CountryModel countryModel = CountryMapper.convertCountryToGrpcModel(country);
+            responseObserver.onNext(countryModel);
+        }
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getCountryById(CountryId request, StreamObserver<CountryModel> responseObserver) {
+        Optional<Country> country = countryRepository.findById(request.getId());
+        if (country.isEmpty()) {
+            com.google.rpc.Status status = com.google.rpc.Status.newBuilder()
+                    .setCode(com.google.rpc.Code.NOT_FOUND.getNumber())
+                    .setMessage("The requested user was not found")
+                    .addDetails(Any.pack(ErrorInfo.newBuilder()
+                            .setReason("User not found")
+                            .build()))
+                    .build();
+            responseObserver.onError(StatusProto.toStatusRuntimeException(status));
+        } else {
+            Country countryFound = country.get();
+            CountryModel countryModel = CountryMapper.convertCountryToGrpcModel(countryFound);
+            responseObserver.onNext(countryModel);
+            responseObserver.onCompleted();
+        }
+    }
+}
