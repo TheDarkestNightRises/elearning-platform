6ea7e52266828021d8841403b76a1804c1258310
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-12-09 01:19:47 +0100

    Country modification application tier setup

---
+++ Elearn-ApplicationTier/Elearn.GrpcService/Extensions/GrpcCountryExtension.cs
@@ -0,0 +1,24 @@
+﻿using Elearn.Shared.Models;
+
+namespace Elearn.GrpcService.Extensions;
+
+public static class GrpcCountryExtension
+{
+    public static CountryModel AsGrpcModel(this Country university)
+    {
+        return new CountryModel()
+        {
+            Id = university.Id,
+            Name = university.Name,
+        };
+    }
+
+    public static Country AsBase(this CountryModel universityModel)
+    {
+        return new Country()
+        {
+            Id = universityModel.Id,
+            Name = universityModel.Name,
+        };
+    }
+}
---
+++ Elearn-ApplicationTier/Elearn.Shared/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace Elearn.Shared.Models;
+
+public class Country
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public Country()
+    {
+        
+    }
+}
