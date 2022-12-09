e3d8118d8a327e2a8d2ca129468ace3afd00d9ab
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2022-12-09 03:18:31 +0100

    Country Http Client

---
+++ Elearn-PresentationTier/Elearn.Clients/Http/CountryHttpClient.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Elearn.HttpClients.Service;
+using Elearn.Shared.Dtos;
+
+namespace Elearn.Clients.Http;
+
+public class CountryHttpClient : ICountryService
+{
+    private readonly HttpClient client;
+
+    public CountryHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+    
+    public async Task<List<CountryDto>> GetAllCountriesAsync()
+    {
+        HttpResponseMessage response = await client.GetAsync("/Countries");
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        List<CountryDto> countryDtos = JsonSerializer.Deserialize<List<CountryDto>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return countryDtos;
+    }
+
+    public async Task<CountryDto> GetCountryByIdAsync(long id)
+    {
+        HttpResponseMessage response = await client.GetAsync($"/Countries/{id}");
+        if (!response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+
+        return await response.Content.ReadFromJsonAsync<CountryDto>();
+    }
+}
--- Elearn-PresentationTier/Elearn.Shared/Dtos/UserDto.cs
+++ Elearn-PresentationTier/Elearn.Shared/Dtos/UserDto.cs
@@ -12,40 +12,14 @@ public class UserDto
     public string Role { get; set; }
 
     public bool Approved { get; set; }
-    public int SecurityLevel
-    {
-        get; set;
-    }
-    public string UniversityName { get; set; }
-    
+    public int SecurityLevel { get; set; }
+    public long UniversityId { get; set; }
+    public long CountryId { get; set; }
 
-    public UserDto(string username, string password, string email, string name, string role, int securityLevel, string universityName)
-    {
-        Username = username;
-        Password = password;
-        Email = email;
-        Name = name;
-        Role = role;
-        SecurityLevel = securityLevel;
-        UniversityName = universityName;
-    }
-    
-    public UserDto(long id, string username, string password, string email, string name, string role, int securityLevel)
-    {
-        Id = id;
-        Username = username;
-        Password = password;
-        Email = email;
-        Name = name;
-        Role = role;
-        SecurityLevel = securityLevel;
-
-
-    }
     public UserDto()
     {
     }
-    
+
     public override string ToString()
     {
         return $"{Id} {Name}";
